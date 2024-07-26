3a42c2b4f8386750c5356653e38d49b891f4d40c
SteaMMe123@ya.ru <SteaMMe123@ya.ru>
2024-07-26 11:06:43 +0700

    final

--- js/main.js
+++ js/main.js
@@ -2,5 +2,16 @@
 var swiper = new Swiper(".swiper-pairs", {
 	slidesPerView: "auto",
 	speed: 1000,
-	spaceBetween:70,
-  });
+	spaceBetween: 70,
+});
+
+const btnBurger = document.querySelector(".burger");
+const btnClose = document.querySelector(".btn-close-menu");
+const menu = document.querySelector(".header__menu");
+
+btnBurger.addEventListener('click', () =>{
+	menu.classList.add('open-menu');
+})
+btnClose.addEventListener('click', () => {
+	menu.classList.remove('open-menu');
+})
